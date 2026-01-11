9cf1e77b9cdf79e61a5954d6b454ab16a5518d9f
rafaeldiasgarcia <rafdgar@gmail.com>
2026-01-11 18:35:02 -0300

    introdução ao window function

---
+++ curso-sql/19_window_function.sql
@@ -0,0 +1,30 @@
+WITH tb_cliente_dia AS (
+
+    SELECT IdCliente,
+           substr(DtCriacao, 1,10) AS dtDia,
+           count(distinct IdTransacao) AS qtdeTransacao
+
+    FROM transacoes
+
+    WHERE dtCriacao >= '2025-08-25'
+      AND dtCriacao < '2025-08-30'
+
+    GROUP BY IdCliente, dtDia
+
+),
+
+     tb_lag AS (
+
+         SELECT *,
+                sum(qtdeTransacao) OVER (PARTITION BY IdCliente ORDER BY dtDia) AS acum,
+                lag(qtdeTransacao) OVER (PARTITION BY IdCliente ORDER BY dtDia) AS lagTransacao
+
+         FROM tb_cliente_dia
+
+     )
+
+SELECT *,
+       1.* qtdeTransacao / lagTransacao
+
+
+FROM tb_lag
---
+++ curso-sql/20_window_function.sql
@@ -0,0 +1,38 @@
+WITH cliente_dia AS (
+
+    SELECT DISTINCT IdCliente,
+                    substr(DtCriacao,1,10) AS dtDia
+
+    FROM transacoes
+    WHERE substr(DtCriacao,1,4) = '2025'
+    ORDER BY IdCliente, dtDia
+
+),
+
+tb_lag AS (
+
+    SELECT *,
+           lag(dtDia) OVER (PARTITION BY IdCliente ORDER BY dtDia) AS lagDia
+    FROM cliente_dia
+
+),
+
+tb_diff_dt AS (
+
+    SELECT *,
+           julianday(dtDia) - julianday(lagDia) AS DtDiff
+    FROM tb_lag
+
+),
+
+avg_cliente AS (
+
+    SELECT IdCliente,
+           avg(DtDiff) AS avgDia
+    FROM tb_diff_dt
+    GROUP BY IdCliente
+
+)
+
+SELECT avg(avgDia)
+FROM avg_cliente
